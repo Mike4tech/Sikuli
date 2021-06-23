49bb9180d82a79ae36cbcf01ea25df588c900304
Mike4tech <8379738+Mike4tech@users.noreply.github.com>
2021-06-23 15:55:58 +0200

    Add files via upload

---
+++ textPusher.py
@@ -0,0 +1,6 @@
+type("l", KeyModifier.CTRL)#direct focus to the address bar
+f.write('<font color= "#347C17">Focus directed to the address bar</font><br />')
+paste("https://www.globaltestmarket.com/")#launch the GMI site
+wait(1)
+type(Key.ENTER)
+
